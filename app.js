ddae48d2834288fbe18623d1754525faff25ca9d
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2021-11-07 21:00:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// document.querySelector('.cart').addEventListener('mouseover', function(){
+//     console.log("MERGE");
+//     document.querySelector('.cart2').style.visibility = 'visible';
+// });
